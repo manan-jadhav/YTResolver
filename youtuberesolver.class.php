ed57f7b3f37ebc2ae0574c5b3811fb084e42a6dc
CurosMJ <curos@cyberdude.com>
2013-01-15 05:25:27 +0530

    final changes

--- youtuberesolver.class.php
+++ youtuberesolver.class.php
@@ -1,17 +1,17 @@
 <?php
 class YouTubeResolverClass
 {
-    public $metadata,$status,$vid,$stream_data,$search_results;
+    public $data,$status,$video_id,$stream_data,$search_results;
     protected $prodata,$rawdata,$streams,$stream,$ch,$rcount,$count,$vurl_exp,$headers,$value;
-    public function get_data($vid)
+    public function get_data($video_id)
     {
-        if(empty($vid) || strlen($vid)!=11)//Check if $vid is set and it's length is 11 characters
+        if(empty($video_id) || strlen($video_id)!=11)//Check if $video_id is set and it's length is 11 characters
         {
             $this->status[]=__METHOD__." : Video ID not set properly";
             return false;
         }
-        $this->metadata['vid']=$vid;
-        $this->ch=curl_init('http://www.youtube.com/get_video_info?video_id='.$vid);
+        $this->data['video_id']=$video_id;
+        $this->ch=curl_init('http://www.youtube.com/get_video_info?video_id='.$video_id);
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);//To get the raw data in a string, we must use this
         $this->rawdata=curl_exec($this->ch);//Save the raw data in $rawdata
         curl_close($this->ch);
@@ -24,27 +24,31 @@ class YouTubeResolverClass
         {
             $this->status[]=__METHOD__." :Successfully fetched video data from YouTube";
         }
-        parse_str($this->rawdata,$this->prodata);//Process the raw data and create an array ($prodata) from it. $prodata stands for "processed data"
+	//Process the raw data and create an array ($prodata) from it. $prodata stands for "processed data"
+        parse_str($this->rawdata,$this->prodata);
         if(!isset($this->prodata['url_encoded_fmt_stream_map']))//Checking if video is protected.
         {
             $this->status[]=__METHOD__." : This video contains content from SME. It is restricted from playback on certain sites. This video can not be downloaded";
             return false;
             break;
         }
-        $this->metadata['title']=$this->prodata['title'];
-        $this->metadata['views']=$this->prodata['view_count'];
-        $this->metadata['duration']=$this->prodata['length_seconds'];
-        $this->metadata['language']=$this->prodata['hl'];
-        $this->metadata['author']=$this->prodata['author'];
-        $this->metadata['thumbnail_url']=$this->prodata['thumbnail_url'];
+        $this->data['title']=$this->prodata['title'];
+        $this->data['views']=$this->prodata['view_count'];
+        $this->data['duration']=$this->prodata['length_seconds'];
+        $this->data['language']=$this->prodata['hl'];
+        $this->data['author']=$this->prodata['author'];
+        $this->data['thumbnail_url']=$this->prodata['thumbnail_url'];
         $this->streams = explode(',',$this->prodata['url_encoded_fmt_stream_map']);//Split the stream map into streams.
-        $this->count=$this->metadata['numstreams']=count($this->streams);
+        $this->count=$this->data['numstreams']=count($this->streams);
         $this->rcount=0;
         while ($this->rcount<$this->count)
         {
-            parse_str($this->streams[$this->rcount],$this->stream);//Select a stream "$stream" from the array "$streams"
-            $this->stream_data[$this->rcount]['url']=$this->stream['url'].'&signature='.$this->stream['sig'];// Create the proper URL
-            $this->stream_data[$this->rcount]['is3D']=$this->stream_data[$this->rcount]['isLive']=$this->stream_data[$this->rcount]['isHD']=false;//Set default value as false
+            
+	//Select a stream "$stream" from the array "$streams"
+	parse_str($this->streams[$this->rcount],$this->stream);
+        // Create the proper URL
+	$this->stream_data[$this->rcount]['url']=$this->stream['url'].'&signature='.$this->stream['sig'];
+        $this->stream_data[$this->rcount]['is3D']=$this->stream_data[$this->rcount]['isLive']=$this->stream_data[$this->rcount]['isHD']=false;//Set default value as false
             switch($this->stream['itag'])//To extract data from itag value (Based on http://en.wikipedia.org/wiki/YouTube#Quality_and_codecs )
             {
                 case '5' : $this->stream_data[$this->rcount]['type']="FLV (.flv) Video";
@@ -288,24 +292,35 @@ class YouTubeResolverClass
             $this->status[]=__METHOD__." : Improper URL was given";
             return false;
         }
-        $this->metadata['vid']=$this->vurl_exp[1];
+        $this->data['video_id']=$this->vurl_exp[1];
         $this->status[]=__METHOD__." : Video ID extracted successfully.";
-        return $this->metadata['vid'];
+        return $this->data['video_id'];
     }
-    public function search_streams($haystack,$needle)
+    public function search_streams($haystack,$needle,$index)
     {
-        $this->count=$this->metadata['numstreams'];
+        $this->count=$this->data['numstreams'];
         while($this->count)
         {
             if($this->stream_data[$this->count][$haystack]==$needle)
             {
                 $this->status[]=__METHOD__." : Successfully found the Key with ".$haystack." as ".$needle;
-                $this->search_results[]=$this->count;
+                $this->search_pre_results[]=$this->count;
             }
             $this->count--;
         }
         if(!empty($this->search_results))
         {
+        	if(isset($index)) 
+        	{
+        		foreach $this->search_pre_results  as $key=>$value
+        		{
+        			foreach $index as $keyin=>$valuein
+        			{
+        				if ($valuein===$value)
+        				{$this->search_results[]=$value;return TRUE;}
+        			}
+        		}
+        	}
             return true;
         }
         $this->status[]=__METHOD__." : Failure in finding Key with ".$haystack." as ".$needle;
